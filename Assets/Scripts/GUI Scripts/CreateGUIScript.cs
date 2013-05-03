00caceae5339459bc1912b2b4e6bd56121413685
Shervin Ghazazani <shervinghazazani@gmail.com>
2013-05-03 16:48:52 -0400

    Revert "fixed a bunch of stuff"
    This reverts commit 99b4e024b3c8739be0cef35a90e1f62ee2f30a72.

--- Assets/Scripts/GUI Scripts/CreateGUIScript.cs
+++ Assets/Scripts/GUI Scripts/CreateGUIScript.cs
@@ -22,13 +22,12 @@ public class CreateGUIScript : MonoBehaviour {
 		//Set label text color to be black
 		GUI.contentColor = Color.black;
 		if(attachedCharacter != null) {
-			GUI.Label(new Rect(100,125,200,30), "Character: " + attachedCharacter.characterName);
-			GUI.Label(new Rect(100,145,100,30), "Health Points: " + attachedCharacter.health.ToString());	
-			GUI.Label(new Rect(100,165,100,30), "Stamina: " + attachedCharacter.stamina.ToString());
+			GUI.Label(new Rect(200,225,200,30), "Character: " + attachedCharacter.characterName);
+			GUI.Label(new Rect(200,245,100,30), "Health Points: " + attachedCharacter.health.ToString());	
+			GUI.Label(new Rect(200,265,100,30), "Stamina: " + attachedCharacter.stamina.ToString());
 			
 			if(attachedCharacter.characterType == CharacterConstants.CharacterType.player) {
 				GUI.contentColor = Color.white;
-				/*
 				if(attachedCharacter.abilityOne != null) buttons[0] = new GUIContent(attachedCharacter.abilityOne.abilityName,attachedCharacter.abilityOne.tooltipText);
 				if(attachedCharacter.abilityTwo != null) buttons[1] = new GUIContent(attachedCharacter.abilityTwo.abilityName,attachedCharacter.abilityTwo.tooltipText);
 				if(attachedCharacter.abilityThree != null) buttons[2] = new GUIContent(attachedCharacter.abilityThree.abilityName,attachedCharacter.abilityThree.tooltipText);
@@ -36,7 +35,7 @@ public class CreateGUIScript : MonoBehaviour {
 				if(attachedCharacter.abilityFive != null) buttons[4] = new GUIContent(attachedCharacter.abilityFive.abilityName,attachedCharacter.abilityFive.tooltipText);
 				
 				selected = GUI.SelectionGrid(new Rect(25,95,110,150),selected,buttons,1);
-				*/
+				
 				if(GUI.Button(new Rect(25,260,110,30),"End Turn")) {
 					attachedCharacter.EndTurn();		
 				}
@@ -62,27 +61,30 @@ public class CreateGUIScript : MonoBehaviour {
 			if(attachedTile.GetFavorList().Count > 0) {
 				int increaseDamageCount = attachedTile.GetNumOfFavorEffectsInTile(ConstantsScript.TileFavorEffect.IncreaseDamage);
 				int decreaseAbilityCostCount = attachedTile.GetNumOfFavorEffectsInTile(ConstantsScript.TileFavorEffect.DecreaseAbilityCost);
+				int increaseHitPercentageCount = attachedTile.GetNumOfFavorEffectsInTile(ConstantsScript.TileFavorEffect.IncreaseHitPercentage);
 				int decreaseEnemyDamageCount = attachedTile.GetNumOfFavorEffectsInTile(ConstantsScript.TileFavorEffect.DecreaseEnemyDamage);
 				int increaseEnemyAbilityCostCount = attachedTile.GetNumOfFavorEffectsInTile(ConstantsScript.TileFavorEffect.IncreaseEnemyAbilityCost);
+				int decreaseEnemyHitPercentageCount = attachedTile.GetNumOfFavorEffectsInTile(ConstantsScript.TileFavorEffect.DecreaseEnemyHitPercentage);
 
 				if(increaseDamageCount > 0) favorText += "Increases player damage by " + increaseDamageCount + "." + Environment.NewLine;
 				if(decreaseAbilityCostCount > 0) favorText += "Decreases player ability cost by " + decreaseAbilityCostCount + " Stamina." + Environment.NewLine;
+				if(increaseHitPercentageCount > 0) favorText += "Increases player hit % by " + increaseHitPercentageCount + " percent." + Environment.NewLine;
 				if(decreaseEnemyDamageCount > 0) favorText += "Decreases enemy damage done by " + decreaseEnemyDamageCount + Environment.NewLine;
 				if(increaseEnemyAbilityCostCount > 0) favorText += "Increases enemy ability cost by " + increaseEnemyAbilityCostCount + " Stamina." +  Environment.NewLine;
+				if(decreaseEnemyHitPercentageCount > 0) favorText += "Decreases enemy hit % by " + decreaseEnemyHitPercentageCount + " percent." +  Environment.NewLine;
 			}
 			
 			if(!favorText.Equals("")) {
 				GUI.contentColor = Color.black;
-				GUI.Label(new Rect(Screen.width - 425,55,300,120),favorText);
+				GUI.Label(new Rect(Screen.width - 325,55,300,120),favorText);
 			}
 			
 			CharacterScript tileInhabitant = attachedTile.GetTileInhabitant();
 			if(tileInhabitant) {
 				GUI.contentColor = Color.black;
 				
-				GUI.Label(new Rect(Screen.width - 125,195,200,30),tileInhabitant.name);
-				GUI.Label(new Rect(Screen.width - 125,235,200,30),"Health: " + tileInhabitant.health.ToString());
-				GUI.Label(new Rect(Screen.width - 125,215,200,30),"Stamina: " + tileInhabitant.staminaMax.ToString());
+				GUI.Label(new Rect(Screen.width - 225,195,200,30),tileInhabitant.name);
+				GUI.Label(new Rect(Screen.width - 225,235,200,30),"Health: " + tileInhabitant.health.ToString());
 			}
 		}
 	}
