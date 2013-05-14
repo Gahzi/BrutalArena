12794368466ca6b2f3214049cf6b5e426ceb3f40
Shervin Ghazazani <shervinghazazani@gmail.com>
2013-05-14 01:34:53 -0400

    Added music and changed end scene buttons

--- Assets/Scripts/Scene Scripts/RestartScript.cs
+++ Assets/Scripts/Scene Scripts/RestartScript.cs
@@ -10,8 +10,12 @@ public class RestartScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void OnGUI () {
-		if (GUI.Button(new Rect(100, 100, 100, 100), "Game Reset")) {
+		if (GUI.Button(new Rect(280, 500, 270, 160), "",GUIStyle.none)) {
 			Application.LoadLevel(1);
+		}
+		
+		if (GUI.Button(new Rect(600, 500, 300, 130), "",GUIStyle.none)) {
+			Application.Quit();
 		}
 			
 	}
